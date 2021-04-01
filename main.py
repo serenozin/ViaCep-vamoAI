e41bbef177bd5f1cbd7ae418e2473f7baedd1887
demetriofragoso <demetrio.fragoso@hotmail.com>
2021-04-01 18:52:04 -0300

    add main

---
+++ main.py
@@ -0,0 +1,9 @@
+cep_consulta = input("Digite o Cep: ")
+    if len(cep_consulta) != 8:
+        print("Erro! Digite 8 digitos [400 Bad Request]")
+
+ UF = input("Digite o estado: ")
+ cidade = input("Digite a cidade: ")   
+ rua = input("Digite a Rua: ")
+
+
