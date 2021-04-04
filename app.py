90fea0f600fc2d219dee9aa38fd5ba9215bc30b4
Vithor <vithortec@gmail.com>
2021-04-03 21:13:46 -0300

    att stylesheets

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import dash_bootstrap_components as dbc
 
 # bootstrap theme
 # https://bootswatch.com/lux/
-external_stylesheets = [dbc.themes.LUX]
+external_stylesheets = ["https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
