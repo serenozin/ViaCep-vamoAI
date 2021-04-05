825a358faf7c2e2ea5eafb306e5b4f287ad968bc
Patryck <patryck@outlook.com>
2021-04-05 00:02:02 -0300

    app config update

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ import dash_bootstrap_components as dbc
 # https://bootswatch.com/lux/
 external_stylesheets = ["https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css"]
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__,
+    title='CEP VamoAI',
+    update_title='Atualizando...',
+    external_stylesheets=external_stylesheets
+    )
+
 
 server = app.server
 app.config.suppress_callback_exceptions = True
