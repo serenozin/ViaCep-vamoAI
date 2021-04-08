9066bcc91ddf1878b41bb07c1d3db9ca136b504c
Patryck <patryck@outlook.com>
2021-04-08 17:07:16 -0300

    refactoring

--- view/cep.py
+++ view/cep.py
@@ -1,9 +1,8 @@
-from dash_bootstrap_components._components.Spinner import Spinner
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output, State
-from dash_extensions.snippets import send_file
+from dash.dependencies import Input, Output
 from dash_extensions import Download
+from dash_extensions.snippets import send_file
 
 from controller import Andress, SearchDownload
 from app import app
@@ -15,40 +14,39 @@ layout = html.Div(
                 dbc.Col(
                     [
                         dbc.Row(
-                            [
-                                # html.Img(src="https://institutomontanari.com.br/wp-content/uploads/2020/05/cadastro-no-ifood.png", height="400px", width="400"),
-                                        
+                            [     
                                 dbc.Card(
                                     [
                                         
                                         html.H3("Busca pelo CEP:"),
                                         html.P(),
                                         dbc.Input(id="input_cep", placeholder="CEP", type="text"),
-                                        
                                         html.P(),
                                         dbc.Row(
-                                            dbc.Col([
-                                                dbc.Spinner(
-                                                        dbc.Collapse(
-                                                        [
-                                                            html.Div(id="status_code_cep"),
-                                                            dbc.Col(id="card_output_cep"),
-                                                            html.P(),
-                                                        ],
-                                                        id="collapse_output_cep",
+                                            dbc.Col(
+                                                [
+                                                    dbc.Spinner(
+                                                            dbc.Collapse(
+                                                            [
+                                                                html.Div(id="status_code_cep"),
+                                                                dbc.Col(id="card_output_cep"),
+                                                                html.P(),
+                                                            ],
+                                                            id="collapse_output_cep",
+                                                        ),
+                                                        type="grow",
+                                                        color="danger"
                                                     ),
-                                                    type="grow",
-                                                    color="danger"
-                                                ),
-                                                 dbc.Col(
-                                                    [
-                                                    html.Div([dbc.Button(".JSON",id='b_download_json_cep', block=True, color="danger", size="sm", outline=True), Download(id='download_json_cep')]),
-                        
-                                                    html.Div([dbc.Button(".CSV",id='b_download_csv_cep', block=True, color="danger", size="sm", outline=True), Download(id='download_csv_cep')])
-                                                    ],
-                                                ),]
+                                                    dbc.Col(
+                                                        [
+                                                        dbc.Button(".JSON",id='b_download_json_cep', block=True, color="danger", size="sm", outline=True), 
+                                                        Download(id='download_json_cep'),
+                                                        dbc.Button(".CSV",id='b_download_csv_cep', block=True, color="danger", size="sm", outline=True), 
+                                                        Download(id='download_csv_cep')
+                                                        ],
+                                                    ),
+                                                ]
                                             ),
-                                            
                                         ),
                                     ],
                                     body=True
@@ -57,7 +55,6 @@ layout = html.Div(
                         ), 
                     ],
                     lg=5,
-                    # align="stretch"
                 ),
                 dbc.Collapse(
                     dbc.Col(
@@ -70,21 +67,16 @@ layout = html.Div(
                                 body=True,
                             ),
                         ),
-                        
-
                     ),
                     id="collapse_mapa_cep",
-                    
                 ),
             ],
             justify="center",
-            align="center",
             className="h-50"
         ),
-        dbc.Row(),
-        dbc.Row(),
     ]
 )
+
 def status_badge(code):
     if code == 200:
         return dbc.Row(dbc.Badge("200 sucesso", color="success"), justify="center")
